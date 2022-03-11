c9c1f25f55b5fb98e0e347ba086f12604cf5f4f0
sebastian raimondo <sebyra@gmail.com>
2022-03-11 14:51:42 -0300

    Create comment listo, me gusta el arrrteeeee!

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -148,4 +148,39 @@ router.put('/unlikes/:id', auth, async (req, res) => {
     }
   });
 
+
+  //Create comment 
+  router.post(
+    "/comment/:id",
+    [auth, body("text", "Text is riquired").not().isEmpty()],
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      try {
+        const user = await User.findById(req.user.id).select("-password");
+        const post = await Post.findById(req.params.id);
+
+        const newComment = {
+          text: req.body.text,
+          name: user.name,
+          avatar: user.avatar,
+          user: req.user.id,
+        };
+
+        console.log(newComment);
+
+        post.comments.unshift(newComment);
+
+        post.save();
+        res.json(post.comments);
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+      }
+    }
+  );
+
+
 module.exports = router;
