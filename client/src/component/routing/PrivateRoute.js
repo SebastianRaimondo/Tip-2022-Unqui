8084504f3793081640428e7d86b8b14bf64dbbee
sebastian raimondo <sebyra@gmail.com>
2022-04-15 14:23:12 -0300

    Agregue una condicion que me falto en la ruta privada

--- client/src/component/routing/PrivateRoute.js
+++ client/src/component/routing/PrivateRoute.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { selectAuthState } from "../../features/userLogginSlice";
+import { selectAuth } from "../../features/userLogginSlice";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  const isAuthenticated = useSelector(selectAuthState);
+  const isAuthenticated = useSelector(selectAuth).isAuthenticated;
+  const isLoading = useSelector(selectAuth).isLoading
 
   //console.log(children)
 
-  if (isAuthenticated) {
+  if (isAuthenticated && !isLoading) {
     return children;
   }
   return <Navigate replace to='/login' />;
