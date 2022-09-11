8076bda30fbb635df7348f584f331f4305aa68c0
Sebastian Raimondo <sebyra@gmail.com>
2022-09-11 17:01:30 -0300

    agregado el link y el icono para nivel de idioma

--- client/src/component/dashboard/DashboardActions.js
+++ client/src/component/dashboard/DashboardActions.js
@@ -9,8 +9,14 @@ export const DashboardActions = () => {
 
         <Link to="/add-experience" className="btn btn-light"
           ><i className="fab fa-black-tie text-primary"></i> Agregar Experiencia</Link>
+
         <Link to="/add-education" className="btn btn-light">   
          <i className="fas fa-graduation-cap text-primary"></i> Agregar Educacion</Link>
+
+         <Link to="/add-language level" className="btn btn-light">   
+         <i className="fas fa-language text-primary"></i> Agregar Nivel de idioma</Link>
+
+
       </div>
   );
 }
