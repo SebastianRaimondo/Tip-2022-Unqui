731384ae22f4a3bb72d539ba299c47310bcb9b84
Sebastian Raimondo <sebyra@gmail.com>
2022-09-30 14:33:32 -0300

    Agregado filtro de busqueda de perfiles

--- client/src/component/profiles/Profiles.js
+++ client/src/component/profiles/Profiles.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, {useEffect, useState } from "react";
 import Spinner from "../layout/Spinner";
 import { useSelector, useDispatch } from "react-redux";
 import { selectProfiles } from "../../features/profileSlice";
@@ -7,13 +7,31 @@ import { selectLoading } from "../../features/profileSlice";
 import ProfileItem from "./ProfileItem"
 
 const Profiles = () => {
+
   const dispatch = useDispatch();
   const loading = useSelector(selectLoading);
   const profiles = useSelector(selectProfiles);
 
+  const [search,setSearch] = useState("")
+ 
+
   useEffect(() => {
     dispatch(getProfiles());
   }, []);
+
+  const searcher = (e) => {
+    setSearch(e.target.value)
+  }
+
+ let results = [] 
+ 
+
+ if(!search){
+  results = profiles
+ }else{
+ results = profiles.filter(pro => pro.skills.includes(search.toLowerCase()))
+ }
+
 
   return (
     <div className="container">
@@ -25,12 +43,22 @@ const Profiles = () => {
           <p className='lead'>
             <i className='fab fa-connectdevelop'></i>
             Busca y conectate con desarrolladores
+
+            
           </p>
 
+          <div className='form'>
+          <input type="text" 
+          placeholder="Buscar por Skills"
+          value={search}
+          onChange={searcher}
+          />  
+          </div>
+          <br></br>
           <div className='profiles'>
-            {profiles.length > 0 ? (
-              profiles.map((profile) => (
-                <ProfileItem key={profile._id} profile={profile} />
+            {results.length > 0 ? (
+              results.map((result) => (
+                <ProfileItem key={result._id} profile={result} />
               ))
             ) : (
               <h4> No se encontraron perfiles</h4>
